44658bcc8eda7b8b009703fd3ef92bad2ed77268
smartph <phucn@mypinpad.com>
2024-09-14 01:05:14 +0700

    chat

--- src/command.ts
+++ src/command.ts
@@ -156,8 +156,8 @@ export default class Command {
             await vscode.commands.executeCommand('workbench.action.chat.openInNewWindow');
             // copy the chat message match[1] to the clipboard
             await vscode.env.clipboard.writeText(chatmatch[1]);
-            await vscode.commands.executeCommand('workbench.action.terminal.chat.focusInput');
-            await delay(100);
+            // await vscode.commands.executeCommand('workbench.action.terminal.chat.focusInput');
+            await delay(1000);
             await vscode.commands.executeCommand('editor.action.clipboardPasteAction');
             // await vscode.commands.executeCommand('workbench.action.terminal.chat.makeRequest');
             return;
